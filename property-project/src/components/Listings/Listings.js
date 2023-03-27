26eb52678a941040d95365cf1073cc4d584d7b28
benjaminmiharris <benjmiharris@gmail.com>
2023-03-27 09:52:15 +0300

    fixed results display css

--- property-project/src/components/Listings/Listings.js
+++ property-project/src/components/Listings/Listings.js
@@ -1,25 +1,24 @@
-import React from 'react'
-import './Listings.css'
-import Column from '../uikit/Column/Column'
-import SortByDropdown from '../uikit/Dropdowns/SortByDropdown/SortByDropdown'
-import { Container } from 'react-bootstrap'
+import React from "react";
+import "./Listings.css";
+import Column from "../uikit/Column/Column";
+import SortByDropdown from "../uikit/Dropdowns/SortByDropdown/SortByDropdown";
+import { Container } from "react-bootstrap";
 
 const Listings = () => {
-    
   return (
-    <Container className='listings'>
-        <Column>
-            <p className="total-listings">1,380 Rental Listings</p>
-            <p className="total-listings-pagination">Showing 8 - 50 results</p>
-        </Column>
-        <Column>
-            <SortByDropdown
-                defaultLabelText={"sort by"}
-                labelBackgroundColor={"#007E8B"}
-            />
-        </Column>
+    <Container className="listings">
+      <div className="total-listings-container">
+        <p className="total-listings">1,380 Rental Listings</p>
+        <p className="total-listings-pagination">Showing 8 - 50 results</p>
+      </div>
+      <Column>
+        <SortByDropdown
+          defaultLabelText={"sort by"}
+          labelBackgroundColor={"#007E8B"}
+        />
+      </Column>
     </Container>
-  )
-}
+  );
+};
 
-export default Listings
+export default Listings;
